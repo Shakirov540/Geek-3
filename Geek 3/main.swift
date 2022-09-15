b89a0f0795b1a1268a9cd1c6143f9301ca4588f7
Aibek <ashakirov981@gmail.com>
2022-09-15 19:25:57 +0600

    no comments

--- Geek 3/main.swift
+++ Geek 3/main.swift
@@ -7,5 +7,70 @@
 
 import Foundation
 
-print("Hello, World!")
+// == -> ravno li
+//> bolshe li
+//< menshe li
+// || ili
+// && i
+// != ne ravno
+// <= menshe ili ravno
+// >= bolshe ili ravno
+
+//Pri vvode 10 raznyh chisel proverit skolko is nih chetnue i skolko nechetnye i skolko is nih bolshe 10 ispolzYa 4 chetnuh 2 nechethuh 1 bolshe 10
+
+
+func plus(num: Int, num2: Int) {
+    if num + num2 > 10{
+        print("chislo bolshe 30")
+    }else{
+print("chislo menshe 10")
+}
+}
+
+//plus(num: 4, num2: 10)
+
+//func numIsqdd(num: Int, num2: Int) {
+//    if (num + num2) % 2 == 0{
+//        print("chislo chetnoe")
+ //   }else{
+ //       print("chislo nechetnoe ili ravno 0")
+    
+
+
+//numIsOdd(num: 3, num2: 3)
+//numIsOdd(num: 2, num2: 1)
+
+func showSeason(num: Int) {
+    if num == 1{
+        print("vesna")
+    }else if num == 2{
+            print("Leto")
+    }else if num == 3{
+        print("Osen")
+    }else if num == 4{
+        print("zima")
+    }else{
+        return
+        }
+    }
+
+showSeason(num: 5)
+showSeason(num: 2)
+showSeason(num: 4)
+
+func newShowSeason(num: Int) {
+    switch num {
+    case 1: print("vesna")
+    case 2: print("leto")
+    case 3: print("osen")
+    case 4: print("zima")
+    default:
+        print("no item found")
+        
+    }
+}
+
+newShowSeason(num: 5)
+newShowSeason(num: 2)
+newShowSeason(num: 4)
 
